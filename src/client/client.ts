a42cf6a2d172b2c40eb70f6c9ba597d7fa6c1ede
Bryan Dellinger <bryan.dellinger.civ@armwarcollege.edu>
2023-04-25 12:45:07 -0400

    added control panel and frame rate

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import {GUI} from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -33,14 +35,49 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cubeFolder = gui.addFolder("Cube")
+const cameraFolder = gui.addFolder("Camera")
+cubeFolder.open()
+cameraFolder.open()
+
+const rotationHelper = {
+    x: cube.rotation.x,
+    y: cube.rotation.y,
+    z: cube.rotation.z,
+  };
+
+  const cameraPositionHelper = {
+    z: camera.position.z,
+  };
+
+
+  const onChange = (axis: 'x' | 'y' | 'z') => (value: number) => {
+    cube.rotation[axis] = value;
+  };
+
+  const onCameraPositionChange = (axis: 'z') => (value: number) => {
+    camera.position[axis] = value;
+  };
+
+  cubeFolder.add(rotationHelper, 'x', 0, Math.PI * 2).onChange(onChange('x'));
+  cubeFolder.add(rotationHelper, 'y', 0, Math.PI * 2).onChange(onChange('y'));
+  cubeFolder.add(rotationHelper, 'z', 0, Math.PI * 2).onChange(onChange('z'));
+
+  cameraFolder.add(cameraPositionHelper, 'z', 0, 20).onChange(onCameraPositionChange('z'));
+
 
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    //cube.rotation.x += 0.01
+    //cube.rotation.y += 0.01
 
     render()
+    stats.update()
 }
 
 function render() {
